54d533662c1747351c58ac65ba909e6aee51c090
KurtCobainTzy <kurtcobaindbagat@gmail.com>
2026-02-08 14:54:28 +0800

    Implement CafeCustomer

---
+++ CafeCustomer.java
@@ -0,0 +1,15 @@
+public class CafeCustomer {
+    private String customerName;
+
+    public CafeCustomer(String name) {
+        this.customerName = name;
+    }
+
+      public String identifyRole() {
+        return "I am the CafeCustomer Kurt Class. I hold the identity of the person placing the order.";
+    }
+
+    public String customerInfo() {
+        return "Customer: " + customerName;
+    }
+}
